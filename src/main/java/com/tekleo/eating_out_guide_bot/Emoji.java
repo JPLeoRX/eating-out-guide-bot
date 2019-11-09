4b0fec78ab49fdeb0d6160968be5968c563fe7f5
Leo <leo.ertuna@gmail.com>
2019-11-09 17:42:46 +0200

    More emojis

--- src/main/java/com/tekleo/eating_out_guide_bot/Emoji.java
+++ src/main/java/com/tekleo/eating_out_guide_bot/Emoji.java
@@ -1,6 +1,19 @@
 package com.tekleo.eating_out_guide_bot;
 
 public enum Emoji {
+    FOOD_GREEN_SALAD("\uD83E\uDD57"),
+    FOOD_TACO("\uD83C\uDF2E"),
+    FOOD_ONION_SOUP("\uD83C\uDF75"),
+    FOOD_CURRY("\uD83C\uDF5B"),
+    FOOD_NOODLES("\uD83C\uDF5C"),
+    FOOD_PASTA("\uD83C\uDF5D"),
+    FOOD_BURGER("\uD83C\uDF54"),
+    FOOD_PIZZA("\uD83C\uDF55"),
+    FOOD_SOUP("\uD83C\uDF72"),
+    FOOD_CAKE_STRAWBERRY("\uD83C\uDF70"),
+    FOOD_SASHIMI("\uD83C\uDF63"),
+    FOOD_FRIES("\uD83C\uDF5F"),
+
     STAR("\u2B50"),
     DOLLAR("\uD83D\uDCB5");
 
--- src/main/java/com/tekleo/eating_out_guide_bot/review/enums/Cuisine.java
+++ src/main/java/com/tekleo/eating_out_guide_bot/review/enums/Cuisine.java
@@ -1,16 +1,19 @@
 package com.tekleo.eating_out_guide_bot.review.enums;
 
+import com.tekleo.eating_out_guide_bot.Emoji;
+
 public enum Cuisine {
-    AMERICAN("American"),
-    JAPANESE("Japanese"),
-    CHINESE("Chinese"),
-    ITALIAN("Italian"),
-    FRENCH("French"),
-    MEXICAN("Mexican"),
-    INDIAN("Indian"),
-    MEDITERRANEAN("Mediterranean"),
-    UKRAINIAN("Ukrainian"),
-    PATISSERIE("Patisserie");
+    AMERICAN("American " + Emoji.FOOD_BURGER.getText()),
+    JAPANESE("Japanese " + Emoji.FOOD_SASHIMI.getText()),
+    CHINESE("Chinese " + Emoji.FOOD_NOODLES.getText()),
+    ITALIAN("Italian " + Emoji.FOOD_PASTA.getText()),
+    FRENCH("French " + Emoji.FOOD_ONION_SOUP.getText()),
+    MEXICAN("Mexican " + Emoji.FOOD_TACO.getText()),
+    INDIAN("Indian " + Emoji.FOOD_CURRY.getText()),
+    MEDITERRANEAN("Mediterranean " + Emoji.FOOD_GREEN_SALAD.getText()),
+    UKRAINIAN("Ukrainian " + Emoji.FOOD_SOUP.getText()),
+    PATISSERIE("Patisserie " + Emoji.FOOD_CAKE_STRAWBERRY.getText()),
+    FASTFOOD("Fast food " + Emoji.FOOD_FRIES.getText());
 
     private String buttonText;
 
