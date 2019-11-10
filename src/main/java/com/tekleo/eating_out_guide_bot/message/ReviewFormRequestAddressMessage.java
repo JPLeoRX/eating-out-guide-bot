f79422430cf76b48921ea3d9b815073158e6f7a7
Leo <leo.ertuna@gmail.com>
2019-11-10 23:41:59 +0200

    Updated request address message

--- src/main/java/com/tekleo/eating_out_guide_bot/message/ReviewFormRequestAddressMessage.java
+++ src/main/java/com/tekleo/eating_out_guide_bot/message/ReviewFormRequestAddressMessage.java
@@ -3,6 +3,6 @@ package com.tekleo.eating_out_guide_bot.message;
 public class ReviewFormRequestAddressMessage implements Message {
     @Override
     public String getText() {
-        return "Адрес: ";
+        return "Адрес или примерное местоположение (район / станция метро): ";
     }
 }
